826d98e32ac4ce1fccec01f885b2ac2139f91387
Elechim01 <mikymanny015@gmail.com>
2021-01-26 22:28:03 +0100

    Initial Commit

---
+++ CustomCamera/CustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraApp.swift
+//  CustomCamera
+//
+//  Created by Michele on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
